dc2fcbc4a12acc6e28f9ee95e50311b2563bfcb6
Obed <obedbadu04@gmail.com>
2022-09-16 10:56:31 +0000

    Introduction

---
+++ 01-Day_Introduction/datatypes.js
@@ -0,0 +1,13 @@
+// using the typeof operator to check different data types
+
+// let firstName = "Kwaku";
+console.log(typeof firstName);
+ 
+// const is_married = false;
+console.log(typeof is_married);
+
+// let lastName;
+console.log(typeof lastName);
+
+// const age = null;
+console.log(typeof age);
---
+++ 01-Day_Introduction/variable.js
@@ -0,0 +1,43 @@
+// Declaring variables and assigning string, boolean, undefined and null data types
+
+// string data type 
+let firstName = "Kwaku";
+
+// boolean data type 
+const is_married = false;
+
+// undefined data type 
+let lastName;
+
+// null data type 
+const age = null;
+
+
+// Declaring four variables without assigning values
+let _gender;
+let bloodGroup;
+let bestLanguage;
+let country;
+
+// Declaring four variables with assigned values
+let gender = "male";
+const myage = 21;
+const _nationality = 'Ghanaian';
+let surName = `Badu`;
+
+// Declaring variables to store my first name, last name, marital status, country and age in multiple lines
+const _firstName = 'Obed';
+const _lastName = 'Badu';
+let maritalStatus = "Single";
+const _country = `Ghana`;
+let $age = 21;
+
+// Declaring variables to store my first name, last name, marital status, country and age in a single line
+let first_name = 'Obed', last_name = 'Badu', marital_tatus = "Single", country1 = `Ghana`, my_age = 21;
+
+// Declaring two variables myAge and yourAge and assigning them initial values and log to the browser console.
+let myAge = 21;
+let yourAge = 35;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
